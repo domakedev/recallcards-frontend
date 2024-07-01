50a1e096777eefe949231dcf8952f7c63ec95bcd
César Guevara Cabrera <sarcansino@gmail.com>
2024-07-01 15:58:26 -0500

    Update image domains allowed

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // domains: ["media.es.wired.com", "placehold.co", "res.cloudinary.com"],
+    domains: ["res.cloudinary.com"],
     // domains: ["media.es.wired.com", "placehold.co", "res.cloudinary.com"],
     //allow all domains
-    remotePatterns: [
-      {
-        hostname: "**",
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     hostname: "**",
+    //   },
+    // ],
   },
   compiler: {
     // Enables the styled-components SWC transform
