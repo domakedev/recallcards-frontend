8490c94b72743d81603503f622a228c34bd5187a
César Guevara Cabrera <sarcansino@gmail.com>
2024-07-09 20:09:37 -0500

    Update Auth buttons color

--- app/create-deck/page.tsx
+++ app/create-deck/page.tsx
@@ -206,8 +206,8 @@ const page = () => {
               disabled={!isAuth}
               className={`${
                 isLoading ? "rainbow-animation" : ""
-              } group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
-                !isAuth ? "cursor-not-allowed" : ""
+              } group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 ${
+                !isAuth ? "cursor-not-allowed bg-gray-600 " : ""
               }`}
             >
               {isAuth
@@ -230,7 +230,7 @@ const page = () => {
             <button
               onClick={() => router.push("/auth/register")}
               disabled={isAuth}
-              className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 my-4`}
+              className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 my-4`}
             >
               Registrarme
             </button>
