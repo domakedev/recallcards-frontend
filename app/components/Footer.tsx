9dafd9746c14b877b3d1796e00f5f5305d09b7ab
César Guevara Cabrera <sarcansino@gmail.com>
2024-07-19 09:23:07 -0500

    Delete Get Cards in Footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,28 +1,20 @@
 "use client";
 
-import { FaHome } from "react-icons/fa";
 import { FaSwatchbook } from "react-icons/fa";
 import { FaLayerGroup } from "react-icons/fa";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { getAllCards } from "@/services/card.services";
-import { Card } from "@/types/Card";
 import { useAppSelector } from "@/redux/hooks";
 import { DeckDB } from "@/types/Deck";
 import { nameToSlug } from "@/utils/nameToSlug";
 import Image from "next/image";
-import { HiMiniHome } from "react-icons/hi2";
 import { GoHomeFill } from "react-icons/go";
 
 
 const Footer = () => {
-  const params = useParams();
   const deckState = useAppSelector((state) => state.deck);
 
-  const [randomCard, setRandomCard] = useState<Card>();
-  const [resetCard, setResetCard] = useState<boolean>(false);
-  const [allCards, setAllCards] = useState<Card[]>([]);
   const [actualDeck, setActualDeck] = useState<DeckDB>();
 
   useEffect(() => {
@@ -35,30 +27,6 @@ const Footer = () => {
       });
     }
   }, [deckState]);
-
-  //To choose a random deck
-  const randomNumber0toNum = (max: number): number => {
-    const number = Math.floor(Math.random() * (max + 1));
-    return number;
-  };
-
-  //To choose a random card from a deck
-  const randomNumber1toNum = (max: number): number => {
-    const number = Math.floor(Math.random() * max) + 1;
-    return number;
-  };
-
-  useEffect(() => {
-    getAllCards().then((data) => setAllCards(data));
-  }, []);
-
-  useEffect(() => {
-    if (allCards?.length > 0) {
-      const randomIndex = randomNumber0toNum(allCards.length - 1);
-      const randomCard: Card = allCards[randomIndex];
-      setRandomCard(randomCard);
-    }
-  }, [resetCard, allCards]);
 
   return (
     <footer className="sticky bottom-0 w-full bg-gray-800 text-white z-20 py-1">
@@ -94,10 +62,8 @@ const Footer = () => {
           </div>
         </Link>
         <Link
-          // href={`/random/${randomCard?.id}`}
           href={"/mis-decks"}
           className="w-1/3 h-full active:bg-gray-700 flex items-center justify-center "
-          onClick={() => setResetCard((state) => !state)}
         >
           <div className="flex flex-col items-center">
             <FaLayerGroup className="text-2xl my-1" />
